0f78d1133d15d016b7d8773826cc747fee18dfc8
ovitan <it.hung.0805@gmail.com>
2014-09-28 12:20:12 +0700

    Create caidat.java

---
+++ caidat.java
@@ -0,0 +1,13 @@
+package com.thoitiet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class caidat extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.caidat);
+	}
+}
