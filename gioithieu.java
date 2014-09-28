7c2d2f27d84299645fd7fd3589ec72253bc652c3
ovitan <it.hung.0805@gmail.com>
2014-09-28 12:21:15 +0700

    Create gioithieu.java

---
+++ gioithieu.java
@@ -0,0 +1,13 @@
+package com.thoitiet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class gioithieu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gioithieu);
+	}
+}
